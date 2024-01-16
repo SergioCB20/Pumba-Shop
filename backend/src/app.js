1da19122d6f2ba12671e6f9197cb4dfa3a0360dd
Sergio <sergiochumbi98@gmail.com>
2024-01-16 00:59:04 -0500

    avance de api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 //Routes
-import router from "./routes/users.routes.js";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 
@@ -10,6 +10,12 @@ app.set("port",4000);
 
 //Middlewares (Son funciones intermedias entre la petición del cliente y la respuesta del servidor)
 app.use(morgan("dev"));//listado de las peticiones (ruta, tipo de peticion, codigo de respuesta)
-app.use(router);
+
+//Cuando acceda a la ruta raiz, va a manejar la request y la response mediante un callback
+app.get("/", (req,res)=>{
+    res.end("<h1>El servidor esta funcionando</h1>")
+})
+
+app.use("/users",userRoutes);
 
 export default app;
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,14 +3,9 @@ import usersController from "../controllers/users.controller.js";
 
 const router = Router();
 
-
-//Cuando acceda a la ruta raiz, va a manejar la request y la response mediante un callback
-router.get("/", (req,res)=>{
-    res.end("<h1>El servidor esta funcionando</h1>")
-})
-router.get("/users",usersController.getUsers);
-router.get("/users/:id",usersController.getUser);
-router.post("/users",usersController.addUser);
-router.put("/users:id",usersController.updateUser);
-router.delete("/users:id",usersController.deleteUser);
+router.get("/",usersController.getUsers);
+router.get("/:id",usersController.getUser);
+router.post("/",usersController.addUser);
+router.put("/:id",usersController.updateUser);
+router.delete("/:id",usersController.deleteUser);
 export default router;
