799b1610d7a16272281ce746c31b733b0f6a2ccc
Sergio <sergiochumbi98@gmail.com>
2024-01-16 01:25:00 -0500

    Retomando Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="container bg-white">
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/Login" element={<Login/>}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
